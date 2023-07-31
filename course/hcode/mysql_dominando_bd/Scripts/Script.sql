67ed05461b00cd66a0fb7f34d72c2def8c30eee1
jsrgyn <jsrgyn@gmail.com>
2023-07-30 22:18:33 -0300

    mysql procedure, views

--- course/hcode/mysql_dominando_bd/Scripts/Script.sql
+++ course/hcode/mysql_dominando_bd/Scripts/Script.sql
@@ -132,7 +132,196 @@ select * from tb_funcionarios tf where cadastro > '20160101';
 select * from tb_funcionarios tf where cadastro > '2016-01-01';
 
 update tb_funcionarios set admissao = CURRENT_DATE() where id = 1;
+
 update tb_funcionarios set admissao = DATE_ADD(CURRENT_DATE(), INTERVAL 60 day) where id = 2 ;
+
+select DATEDIFF(admissao, CURRENT_DATE()) as `diferença de dias` from tb_funcionarios where id = 2;
+
+select * from tb_funcionarios tf where MONTH(admissao) = 7;
+
+-- 16. A Cláusula Order by e Limit
+
+select * from tb_funcionarios tf order by name;
+
+select * from tb_funcionarios tf order by 2;
+
+select * from tb_funcionarios tf order by salario desc, name;
+
+select * from tb_funcionarios tf order by salario limit 0, 3;
+
+select * from tb_funcionarios tf where year(cadastro) = 2016 and MONTH(cadastro) = 5
+order by salario LIMIT 0, 3;
+
+select * from tb_funcionarios tf where year(admissao) = 2016 and MONTH(admissao) = 5
+order by salario LIMIT 0, 3;
+
+
+-- 18. Dominando o comando Update
+
+update tb_funcionarios 
+set salario = 3000
+where id = 5;
+
+select * from tb_funcionarios tf where id = 5;
+
+UPDATE tb_funcionarios 
+SET salario = 4000, admissao = '2015-12-20'
+where
+  id = 6;
+ 
+select * from tb_funcionarios tf WHERE id = 6;
+
+-- 19. Dominando o comando Delete
+
+delete from tb_funcionarios where id = 1;
+
+start transaction;
+
+delete from tb_pessoas;
+
+SELECT * from tb_pessoas tp;
+
+rollback;
+
+commit;
+
+insert into tb_pessoas values(null, 'José', 'M');
+
+truncate table tb_pessoas;
+
+-- 20. Criando e relacionando as tabelas
+
+drop table tb_funcionarios;
+
+drop table tb_pessoas;
+
+CREATE table tb_pessoas (
+	idpessoa int auto_increment not null,
+	desnome varchar(255) not null,
+	dt_cadastro timestamp not null default current_timestamp(),
+	constraint PK_pessoas primary key (idpessoa)
+) engine = innoDB;
+
+create table tb_funcionarios (
+	idfuncionario int auto_increment not null,
+	idpessoa int not null,
+	vlsalario decimal(10,2) not null default 1000.00,
+	dtadmissao date not null,
+	constraint Pk_funcionario primary key (idfuncionario),
+	constraint Fk_funcionario_pessoas foreign key (idpessoa)
+		references tb_pessoas (idpessoa)
+) engine = innoDB;
+
+insert into tb_pessoas values(null, 'João', null);
+
+select * from tb_pessoas tp;
+
+INSERT into tb_funcionarios values(null, 1, 5000, current_date());
+
+INSERT into tb_funcionarios values(null, 2, 5000, current_date());
+
+-- 21. Criando Consultas Avançadas com Joins (vídeo e PDF)
+
+SELECT * 
+FROM tb_funcionarios a
+inner join tb_pessoas b on a.idpessoa = b.idpessoa;
+
+select * 
+from tb_funcionarios tf 
+inner join tb_pessoas tp using(idpessoa);
+
+INSERT into tb_pessoas values(null, 'Glaucio', current_date());
+
+select * 
+from tb_pessoas a 
+-- left join tb_pessoas b  using(idpessoa);
+left join tb_funcionarios b on a.idpessoa = b.idpessoa;
+
+select * 
+from tb_pessoas a 
+right join tb_funcionarios b on a.idpessoa = b.idpessoa;
+
+
+-- 22. Criando subconsultas com subquery
+
+user hcode;
+
+insert into tb_pessoas values(null, 'Jose', current_date());
+
+select idpessoa from tb_pessoas tp where desnome like 'J%';
+
+delete from tb_pessoas where idpessoa in (select idpessoa from tb_pessoas tp where desnome like 'J%');
+
+-- 23. A cláusula Group By
+
+CREATE TABLE tb_pedidos (
+  idpedido INT auto_increment NOT NULL,
+  idpessoa INT NOT NULL,
+  dtpedido DATETIME NOT NULL,
+  vlpedido DEC(10,2),
+  constraint PK_pedidos PRIMARY KEY(idpedido),
+  constraint FK_pedidos_pessoas foreign key (idpessoa) references tb_pessoas (idpessoa)
+);
+
+
+select * from tb_pessoas tp;
+
+insert into tb_pedidos value(null, 1, current_date(), 2300.00);
+insert into tb_pedidos value(null, 1, current_date(), 5000);
+insert into tb_pedidos value(null, 1, current_date(), 10000);
+insert into tb_pedidos value(null, 1, current_date(), 3000);
+
+insert into tb_pedidos value(null, 2, current_date(), 1999.90);
+insert into tb_pedidos value(null, 2, current_date(), 2000);
+insert into tb_pedidos value(null, 2, current_date(), 123.45);
+
+insert into tb_pedidos value(null, 3, current_date(), 4000);
+
+select 
+  b.desnome, 
+  sum(a.vlpedido) as total,
+  CONVERT(avg(a.vlpedido), dec(10,2)) as media,
+  convert(min(a.vlpedido), dec(10.2)) as 'menor valor',
+  convert(max(a.vlpedido), dec(10.2)) as 'maior valor',
+  count(*) as 'Total de pedidos'
+from tb_pedidos a inner join tb_pessoas b using(idpessoa) 
+group by b.idpessoa
+order by sum(a.vlpedido);	
+
+
+-- 24. Filtrando grupos com having
+select 
+  b.desnome, 
+  sum(a.vlpedido) as total,
+  CONVERT(avg(a.vlpedido), dec(10,2)) as media,
+  convert(min(a.vlpedido), dec(10.2)) as 'menor valor',
+  convert(max(a.vlpedido), dec(10.2)) as 'maior valor',
+  count(*) as 'Total de pedidos'
+from tb_pedidos a inner join tb_pessoas b using(idpessoa) 
+group by b.idpessoa
+having sum(a.vlpedido) > 6200 
+order by sum(a.vlpedido);	
+
+
+-- 25. Criando views
+
+create view v_pedidostotais 
+as 
+select 
+  b.desnome, 
+  sum(a.vlpedido) as total,
+  CONVERT(avg(a.vlpedido), dec(10,2)) as media,
+  convert(min(a.vlpedido), dec(10.2)) as 'menor valor',
+  convert(max(a.vlpedido), dec(10.2)) as 'maior valor',
+  count(*) as 'Total de pedidos'
+from tb_pedidos a inner join tb_pessoas b using(idpessoa) 
+group by b.idpessoa 
+order by sum(a.vlpedido);	
+
+select * from v_pedidostotais where total > 7000;
+
+-- 26. Trabalhando com Stored Procedures
+
 
 
 

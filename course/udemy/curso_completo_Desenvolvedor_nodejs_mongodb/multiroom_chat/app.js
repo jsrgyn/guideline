a4490bb069b625ddef97bc78eaf205f4d79d6564
jsrgyn <jsrgyn@gmail.com>
2023-08-18 23:30:36 -0300

    Configurando o socket.io
    Socket.io

--- course/udemy/curso_completo_Desenvolvedor_nodejs_mongodb/multiroom_chat/app.js
+++ course/udemy/curso_completo_Desenvolvedor_nodejs_mongodb/multiroom_chat/app.js
@@ -12,6 +12,8 @@ var server = app.listen(80, function(){
 
 var io = require('socket.io').listen(server);
 
+app.set('io', io)
+
 /* Criar a conexão por websocket */
 io.on('connection', function(socket){
   console.log('Usuário conectou');
--- course/udemy/curso_completo_Desenvolvedor_nodejs_mongodb/multiroom_chat/app/controllers/chat.js
+++ course/udemy/curso_completo_Desenvolvedor_nodejs_mongodb/multiroom_chat/app/controllers/chat.js
@@ -14,7 +14,15 @@ module.exports.iniciaChat = function(application, req, res) {
     res.render('index', {validacao: erros})
     return;
   }
+/* 
+  application.get('io').emit(
+    'msgParaCliente', 
+    'Teste');
+ */
 
+    application.get('io').emit(
+      'msgParaCliente', 
+      {apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat'});
 
   res.render('chat')
 }

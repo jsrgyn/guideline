b1655ade759ab5a722a504d54fb4254c818adebd
jsrgyn <jsrgyn@gmail.com>
2023-08-19 23:15:34 -0300

    Configurando o chat pelo socket.io

--- course/udemy/curso_completo_Desenvolvedor_nodejs_mongodb/multiroom_chat/app.js
+++ course/udemy/curso_completo_Desenvolvedor_nodejs_mongodb/multiroom_chat/app.js
@@ -21,4 +21,9 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('Usuário desconectou');
   });
+
+  socket.on('msgParaServidor', function(data){
+    socket.emit('msgParaCliente', {apelido: data.apelido; mensagem: data.mensagem})
+
+  })
 })

de56b6bafbc39f6609947bbb307d2da3ba6028fc
jsrgyn <jsrgyn@gmail.com>
2023-10-12 22:37:07 -0300

    Estundando streams com node

--- course/rocketseat/ignite/node-js/01-fundamentos-nodejs/streams/fundamentals.js
+++ course/rocketseat/ignite/node-js/01-fundamentos-nodejs/streams/fundamentals.js
@@ -15,5 +15,55 @@
 
 // Streams →
 
-process.stdin.pipe(process.stdout)
+// process.stdin.pipe(process.stdout)
+
+import { Readable, Writable, Transform } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+  index = 1
+  
+  _read() {
+    const i = this.index++
+
+/*     if (i > 100) {
+      this.push(null)
+    } else {
+      const buf = Buffer.from(String(i))
+
+      this.push(buf)
+    } */
+
+    setTimeout(()=> {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+  
+        this.push(buf)
+      }
+    }, 1000)
+
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+
+}
+
+new OneToHundredStream()
+  //.pipe(process.stdout)
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
 
